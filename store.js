d1222c2d02b1b436fabbb6539a2c4d800a3290b9
Yusuf Ahmad <yusufahmad5885@gmail.com>
2023-01-09 19:44:38 -0800

    added function to update cart

---
+++ store.js
@@ -0,0 +1,27 @@
+var removeCartItemButtons = document.getElementsByClassName("btn-danger")
+console.log(removeCartItemButtons)
+
+for (var i = 0; i < removeCartItemButtons.length; i++){
+    var button = removeCartItemButtons[i]
+    button.addEventListener('click', function(event) {
+        var buttonClicked = event.target
+        buttonClicked.parentElement.parentElement.remove()
+        updateCartTotal()
+    })
+}
+
+function updateCartTotal() {
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    var total = 0
+    for (var i = 0; i < cartRows.length; i++){
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        console.log(priceElement, quantityElement)
+        var price = parseFloat(priceElement.innerText.replace('$',''))
+        var quantity = quantityElement.value
+        console.log(price *quantity)
+    }
+
+}
